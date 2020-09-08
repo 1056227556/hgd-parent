883d062219632d626f52ffe2236102f1073e1f9d
柳絮江南 <1056227556@qq.com>
2020-09-08 16:51:39 +0800

    cloud主体框架搭建

---
+++ hgd-eureka/hgd-eureka-01/src/main/java/com/lxjn/hgd/eureka/Eureka01Application.java
@@ -0,0 +1,13 @@
+package com.lxjn.hgd.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Eureka01Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Eureka01Application.class,args);
+    }
+}
